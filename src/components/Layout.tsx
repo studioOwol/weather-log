1b899a8a11944a95d45b9152fb638498decc6e11
Nahyun <from.owol@gmail.com>
2025-08-21 16:16:45 +0900

    refactor: relocate files directory

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 interface LayoutProps {
   children: ReactNode
@@ -10,6 +11,8 @@ export default function Layout({ children }: LayoutProps) {
     <div className="min-h-screen bg-background flex flex-col">
       <Header />
       <main>{children}</main>
+
+      <Footer />
     </div>
   )
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/ui/Layout'
+import Layout from '@/components/Layout'
 import { Outlet } from 'react-router'
 
 export default function RootLayout() {
