bdf64f7d10a5182401d17916cd85bc843fb69c89
Nahyun <from.owol@gmail.com>
2025-08-21 14:32:31 +0900

    style: setup design system

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,14 @@
 export default {
   singleQuote: true,
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   printWidth: 100,
   useTabs: false,
   tabWidth: 2,
-  endOfLine: "lf",
+  endOfLine: 'lf',
   bracketSameLine: false,
-  arrowParens: "always",
-};
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+}
