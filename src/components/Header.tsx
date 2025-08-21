d96765a5b6cb0d1f3431dc8f40d4a1a0430e43c3
Nahyun <from.owol@gmail.com>
2025-08-21 18:36:45 +0900

    style: modify theme colors for Tailwind CSS v4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b">
-      <div className="h-16 p-4 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Cloud className="size-6" />
-          <h1 className="text-xl font-bold text-primary">Weather Log</h1>
+    <header className='sticky top-0 z-50 w-full border-b border-border-default'>
+      <div className='h-16 p-4 flex items-center justify-between text-primary'>
+        <div className='flex items-center gap-2'>
+          <Cloud className='size-6' />
+          <h1 className='text-xl font-bold'>Weather Log</h1>
         </div>
 
-        <nav className="flex items-center gap-4">
-          <Link to="/" className="flex items-center gap-2 text-xl font-bold">
-            <Home className="size-5" />
-            <span className="text-[16px]">Home</span>
+        <nav className='flex items-center gap-4'>
+          <Link to='/' className='flex items-center gap-2 text-xl font-bold text-primary'>
+            <Home className='size-5' />
+            <span className='text-[16px]'>Home</span>
           </Link>
 
-          <Link to="/bookmark" className="flex items-center gap-1 text-xl font-bold">
-            <Bookmark className="size-5" />
-            <span className="text-[16px]">Bookmark</span>
+          <Link to='/bookmark' className='flex items-center gap-1 text-xl font-bold text-primary'>
+            <Bookmark className='size-5' />
+            <span className='text-[16px]'>Bookmark</span>
           </Link>
         </nav>
       </div>
